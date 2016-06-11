be67d0f22666067288f5adcc28fd333cf40bc296
kentaro <kiwi8989@gmail.com>
2016-06-11 14:41:26 +0900

    お気に入り授業１位から５位までを集計＆重み付け

--- calc.php
+++ calc.php
@@ -10,18 +10,60 @@ $user_zemis = array(
     'zemi2' => 2,
     'zemi3' => 3
 );
-//DBからuser_zemis[zemi1]と同じゼミを第１志望にしているユーザーを取得
+//DBからuser_zemis[zemi1]と同じゼミを第１志望にしているユーザーとその人が登録している科目を取得
 $stmt = $pdo->query("SELECT classes.user_id, classes.class1, classes.class2, classes.class3, classes.class4, classes.class5 FROM classes INNER JOIN zemis ON classes.user_id = zemis.user_id WHERE zemis.zemi1 = $user_zemis[zemi1]");
 $classIdsByZemi1 = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($classIdsByZemi1);
+//var_dump($classIdsByZemi1);
 //
-$sum = [];
+$sum = array();
 foreach($classIdsByZemi1 as $classId){
     if(count($classId) == 0){
         continue;
     }
+    $class1 = $classId["class1"];
+    $class2 = $classId["class2"];
+    $class3 = $classId["class3"];
+    $class4 = $classId["class4"];
+    $class5 = $classId["class5"];
+    if(empty($sum[$class1])){
+        $sum += array(
+            $class1 => 5
+        );
+    }else{
+        $sum[$class1] = $sum[$class1] + 5;
+    }
+    if(empty($sum[$class2])){
+        $sum += array(
+            $class2 => 4
+        );
+    }else{
+        $sum[$class2] = $sum[$class2] + 4;
+    }
+    if(empty($sum[$class3])){
+        $sum += array(
+            $class3 => 3
+        );
+    }else{
+        $sum[$class3] = $sum[$class3] + 3;
+    }
+    if(empty($sum[$class4])){
+        $sum += array(
+            $class4 => 2
+        );
+    }else{
+        $sum[$class4] = $sum[$class4] + 2;
+    }
+    if(empty($sum[$class5])){
+        $sum += array(
+            $class5 => 1
+        );
+    }else{
+        $sum[$class5] = $sum[$class5] + 1;
+    }
+    //$sum[$class2] = $sum[$class2] + 4;
 }
+var_dump($sum);
 foreach ($zemis as $zemi) {
     $base = $classIdsByZemi1;
     if (count($base) === 0) {
